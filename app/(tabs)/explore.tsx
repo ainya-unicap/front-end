7fc24073d0603967e1250b8147260f2c3ccc8ceb
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2026-03-24 00:28:42 -0300

    upload: adiciona arquivos do repositório antigo

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, View, Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View className='py-10 px-6'>
+      <Text className='text-2xl font-semibold'>Explore</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className='py-10 px-6'>
+      <Text className='text-2xl font-semibold'>Home</Text>
+    </View>
+  );
+}
